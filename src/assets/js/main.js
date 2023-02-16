861f57b7094f9fd8deea78d867a19702ff38b423
Paulo Kurpel <108092490+PauloKurpel@users.noreply.github.com>
2023-02-16 11:04:01 -0300

    form direcionamento

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,3 +15,21 @@ function exibircadastro(){
 
   return resultado
 }
+
+function exibirdirecionamento(){
+  var resultado = Swal.fire({
+      icon: 'question',
+      html:
+      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_direcionamento").value + '<b style="color: #186861;"> solicita agendamento para: </b>' + document.getElementById("dtagen_direcionamento").value + '; </br>' +
+      '<b style="color: #186861;">Consultas(especialidade)/exames/procedimentos: </b>' + document.getElementById("consulta_direcionamento").value + '; </br>' +
+      '<b style="color: #186861;">Locais informados: </b>' + document.getElementById("locais_direcionamento").value + '; </br>' +
+      '<b style="color: #186861;">Cidade/estado: </b>' + document.getElementById("cidade_direcionamento").value + '; </br>' +
+      '<b style="color: #186861;">Enviado para (e-mail/telefone/sms): </b>' + document.getElementById("envio_direcionamento").value + '; </br>' +
+      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_direcionamento").value + '; </br>' ,
+      showCloseButton: false,
+      showConfirmButton: false,
+      width: 700
+  })
+
+  return resultado
+}
