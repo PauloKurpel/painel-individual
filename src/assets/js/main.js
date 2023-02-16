4d407c9006188e9d2de2d1ea4e17dd254e9382f6
Paulo Kurpel <108092490+PauloKurpel@users.noreply.github.com>
2023-02-16 11:40:48 -0300

    add forms

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -33,3 +33,38 @@ function exibirdirecionamento(){
 
   return resultado
 }
+
+
+function exibirservico(){
+  var resultado = Swal.fire({
+      icon: 'question',
+      html:
+      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_servico").value + '<b style="color: #186861;"> solicita agendamento para: </b>' + document.getElementById("dtagen_servico").value + '<b style="color: #186861;">, mas não foi localizado a especialidade/procedimento desejado. Ciente da abertura desta demanda.</b>' + '</br>' +
+      '<b style="color: #186861;">Consultas(especialidade)/exames/procedimentos: </b>' + document.getElementById("consulta_servico").value + '; </br>' +
+      '<b style="color: #186861;">Cidade/estado: </b>' + document.getElementById("cidade_servico").value + '; </br>' +
+      '<b style="color: #186861;">Enviado para (e-mail/telefone/sms): </b>' + document.getElementById("envio_servico").value + '; </br>' +
+      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_servico").value + '; </br>' ,
+      showCloseButton: false,
+      showConfirmButton: false,
+      width: 700
+  })
+
+  return resultado
+}
+
+function exibirflowrn(){
+  var resultado = Swal.fire({
+      icon: 'question',
+      html:
+      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_flowrn").value + '<b style="color: #186861;"> solicita agendamento para: </b>' + document.getElementById("dtagen_flowrn").value + '<b style="color: #186861;">, mas não haviam agendas disponíveis em agendamento e direcionamento conforme informado pelo beneficiário. Ciente da abertura desta demanda.</b>' + '</br>' +
+      '<b style="color: #186861;">Consultas(especialidade)/exames/procedimentos: </b>' + document.getElementById("consulta_flowrn").value + '; </br>' +
+      '<b style="color: #186861;">Cidade/estado: </b>' + document.getElementById("cidade_flowrn").value + '; </br>' +
+      '<b style="color: #186861;">Enviado para (e-mail/telefone/sms): </b>' + document.getElementById("envio_flowrn").value + '; </br>' +
+      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_flowrn").value + '; </br>' ,
+      showCloseButton: false,
+      showConfirmButton: false,
+      width: 700
+  })
+
+  return resultado
+}
