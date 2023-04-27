fb28e79172e2d5643fb90f5d1a01778197be5604
Paulo Kurpel <108092490+PauloKurpel@users.noreply.github.com>
2023-04-27 18:01:24 -0300

    alteracoes-0423

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,15 +2,15 @@ function exibircadastro(){
   var resultado = Swal.fire({
       icon: 'question',
       html:
-      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_benef").value + '<b style="color: #186861;"> solicita agendamento para: </b>' + document.getElementById("consulta_benef").value + '; </br>' +
-      '<b style="color: #186861;">Consultas/exames/procedimentos: </b>' + document.getElementById("consulta_benef").value + '; </br>' +
-      '<b style="color: #186861;">Agendado em: </b>' + document.getElementById("dtageem_benef").value + '; </br>' +
-      '<b style="color: #186861;">Bairro/cidade/estado/município: </b>' + document.getElementById("bairro_benef").value + '; </br>' +
-      '<b style="color: #186861;">Enviado para (e-mail/telefone/sms): </b>' + document.getElementById("envio_benef").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_benef").value + '; </br>' ,
+      '<b style="color: #186861;">Senhor(a) </b>' + document.getElementById("nome_benef").value + '<b style="color: #186861;"> solicita agendamento para especialidade </b>' + document.getElementById("consulta_benef").value +
+      '<b style="color: #186861;">, agendado com o profissional </b>' + document.getElementById("profissional_benef").value +
+      '<b style="color: #186861;"> na especialidade </b>' + document.getElementById("consulta_benef").value +
+      '<b style="color: #186861;">, data/hora: </b>' + document.getElementById("dtageem_benef").value +
+      '<b style="color: #186861;">, procedimento: </b>' + document.getElementById("procedimento_benef").value +
+      '<br><b style="color: red;">Necessário comparecer com 15 minutos de antecedência portando máscara, documento de identificação e carteirinha do convênio.</b>' ,
       showCloseButton: false,
       showConfirmButton: false,
-      width: 700
+      width: 900
   })
 
   return resultado
@@ -20,15 +20,13 @@ function exibirdirecionamento(){
   var resultado = Swal.fire({
       icon: 'question',
       html:
-      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_direcionamento").value + '<b style="color: #186861;"> solicita agendamento para: </b>' + document.getElementById("consulta_direcionamento").value + '; </br>' +
-      '<b style="color: #186861;">Consultas(especialidade)/exames/procedimentos: </b>' + document.getElementById("consulta_direcionamento").value + '; </br>' +
-      '<b style="color: #186861;">Locais informados: </b>' + document.getElementById("locais_direcionamento").value + '; </br>' +
-      '<b style="color: #186861;">Cidade/estado: </b>' + document.getElementById("cidade_direcionamento").value + '; </br>' +
-      '<b style="color: #186861;">Enviado para (e-mail/telefone/sms): </b>' + document.getElementById("envio_direcionamento").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_direcionamento").value + '; </br>' ,
+      '<b style="color: #186861;">Senhor(a) </b>' + document.getElementById("nome_direcionamento").value + '<b style="color: #186861;"> solicita indicação de prestadores, referente a especialidade </b>' + document.getElementById("consulta_direcionamento").value +
+      '<b style="color: #186861;"> na região de </b>' + document.getElementById("cidade_direcionamento").value +
+      '<b style="color: #186861;">. Realizado indicação. Encaminhado via e-mail/SMS: </b>' + document.getElementById("envio_direcionamento").value +
+      '<br><b style="color: red;">Ciente que a rede de atendimento também está disponível via site/app. Ciente também que a validade da guia tem o prazo de 90 dias, precisa ter a indicação clinica ou hipótese diagnóstica.</b>' ,
       showCloseButton: false,
       showConfirmButton: false,
-      width: 700
+      width: 900
   })
 
   return resultado
@@ -73,13 +71,12 @@ function exibirfinanceiroinfo(){
   var resultado = Swal.fire({
       icon: 'question',
       html:
-      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_financeiroinfo").value + '<b style="color: #186861;"> solicita informações sobre seu(s) boleto(s).</b> </br>' +
-      '<b style="color: #186861;">Mês: </b>' + document.getElementById("mes_financeiroinfo").value + '; </br>' +
-      '<b style="color: #186861;">Vencimento: </b>' + document.getElementById("vcto_financeiroinfo").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento: </b>' + document.getElementById("obs_financeiroinfo").value + '; </br>' ,
+      '<b style="color: #186861;">Senhor(a) </b>' + document.getElementById("nome_financeiroinfo").value +
+      '<b style="color: #186861;"> solicita informações sobre o valor maior do seu boleto do mês </b>' + document.getElementById("mes_financeiroinfo").value + '. ' +
+      '<b style="color: #186861;">Informada que a ANS, definiu que os planos de saúde individuais e familiares médico-hospitalares regulamentados terão reajuste de 15,5%, referente ao percentual definido no ano de 2022.</b>' ,
       showCloseButton: false,
       showConfirmButton: false,
-      width: 700
+      width: 1000
   })
 
   return resultado
@@ -89,14 +86,14 @@ function exibirfinanceiroboleto(){
   var resultado = Swal.fire({
       icon: 'question',
       html:
-      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_financeiroboleto").value + '<b style="color: #186861;"> solicita informações sobre seu(s) boleto(s).</b> </br>' +
-      '<b style="color: #186861;">Mês: </b>' + document.getElementById("mes_financeiroboleto").value + '; </br>' +
-      '<b style="color: #186861;">Vencimento: </b>' + document.getElementById("vcto_financeiroboleto").value + '; </br>' +
-      '<b style="color: #186861;">Enviado para (e-mail/telefone/sms): </b>' + document.getElementById("envio_financeiroboleto").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento: </b>' + document.getElementById("obs_financeiroboleto").value + '; </br>' ,
+      '<b style="color: #186861;">Senhor(a) </b>' + document.getElementById("nome_financeiroboleto").value +
+      '<b style="color: #186861;"> solicita 2ª via do boleto / informação sobre boleto da competência </b>' + document.getElementById("competencia_financeiroboleto").value + '. ' +
+      '<b style="color: #186861;">Encaminhado via e-mail/SMS: </b>' + document.getElementById("envio_financeiroboleto").value + '. ' +
+      '<b style="color: #186861;">Vencimento no dia </b>' + document.getElementById("vcto_financeiroboleto").value +
+      '<br><b style="color: red;">Ciente que segunda via de boleto também está disponível via site/app.</b>',
       showCloseButton: false,
       showConfirmButton: false,
-      width: 700
+      width: 900
   })
 
   return resultado
@@ -110,10 +107,11 @@ function exibirreembolso(){
       '<b style="color: #186861;">Mês: </b>' + document.getElementById("mes_reembolso").value + '; </br>' +
       '<b style="color: #186861;">Protocolo: </b>' + document.getElementById("protocolo_reembolso").value + '; </br>' +
       '<b style="color: #186861;">Status: </b>' + document.getElementById("status_reembolso").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento: </b>' + document.getElementById("obs_reembolso").value + '; </br>' ,
+      '<b style="color: #186861;">Obs. do atendimento: </b>' + document.getElementById("obs_reembolso").value + '; </br>' +
+      '<br><b style="color: red;">Cliente ciente à respeito do processo de análise e prazo de reembolso, conforme clausulas contratuais.</b>',
       showCloseButton: false,
       showConfirmButton: false,
-      width: 700
+      width: 900
   })
 
   return resultado
@@ -123,11 +121,10 @@ function exibirghi(){
   var resultado = Swal.fire({
       icon: 'question',
       html:
-      '<b style="color: #186861;">Sra(o) </b>' + document.getElementById("nome_ghi").value + '<b style="color: #186861;"> solicita informações sobre GIH.</b> </br>' +
-      '<b style="color: #186861;">Data de solicitação: </b>' + document.getElementById("dtsoli_ghi").value + '; </br>' +
-      '<b style="color: #186861;">Consultas/exames/procedimentos: </b>' + document.getElementById("consulta_ghi").value + '; </br>' +
-      '<b style="color: #186861;">Status: </b>' + document.getElementById("status_ghi").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento: </b>' + document.getElementById("obs_ghi").value + '; </br>' ,
+      '<b style="color: #186861;">Senhor(a) </b>' + document.getElementById("nome_ghi").value +
+      '<b style="color: #186861;"> solicita informações sobre o pedido de GIH </b>' + document.getElementById("consulta_ghi").value +
+      '<b style="color: #186861;">, com a data de solicitação: </b>' + document.getElementById("dtsoli_ghi").value + '. ' +
+      '<b style="color: #186861;">Oriento que o pedido está em tratativa. Prazo de 21 dias úteis.</b>' ,
       showCloseButton: false,
       showConfirmButton: false,
       width: 700
@@ -146,7 +143,8 @@ function exibirtoken(){
       '<b style="color: #186861;">Data de nascimento: </b>' + document.getElementById("nasc_token").value + '; </br>' +
       '<b style="color: #186861;">Telefone: </b>' + document.getElementById("telefone_token").value + '; </br>' +
       '<b style="color: #186861;">E-mail: </b>' + document.getElementById("email_token").value + '; </br>' +
-      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_token").value + '; </br>' ,
+      '<b style="color: #186861;">Obs. do atendimento (se necessário): </b>' + document.getElementById("obs_token").value + '; </br>' +
+      '<b style="color: red;">Beneficiário ciente que o token está disponível também através do site ou do aplicativo.</b>' ,
       showCloseButton: false,
       showConfirmButton: false,
       width: 900
